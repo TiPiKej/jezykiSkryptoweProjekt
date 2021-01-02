d4e54caba850615adfe6abf4a06d45f098c76d55
Tomasz <40639418+TiPiKej@users.noreply.github.com>
2021-01-02 17:47:40 +0100

    Update menu.py

--- components/menu.py
+++ components/menu.py
@@ -8,7 +8,3 @@ menu_option = [
     {"name": "Eksportuj dane z pliku xlsx do pliku JSON", "function": save_xlsx_to_json},
     {"name": "Importuj dane z pliku JSON do pliku xlsx", "function": save_json_to_xlsx}
 ]
-
-# to do
-# mam 2 tabele -> szukam wpisow unikalnych 
-# eksport tabeli do formatu bazodanowego
